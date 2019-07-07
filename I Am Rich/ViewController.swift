58aff2065ad1b07873763277e1cb6d48f306ce24
Aaron Paul <a29paul@uwaterloo.ca>
2019-07-06 22:21:27 -0400

    add other files

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Aaron Paul on 2019-03-04.
+//  Copyright © 2019 Aaron Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
